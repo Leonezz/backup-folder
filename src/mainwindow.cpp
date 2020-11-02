7d88dff8db02d36d341f0ef24fdbf1e283c7f5b0
Leonezz <lonelywolf12138@Outlook.com>
2020-11-02 11:06:59 +0800

    :construction:(mainwindow): create system folder C://ProgramData/BackupFolders on windows & /usr/share/BackupFolders on Linux.Tasks will be stored in file BackupFolders/tasks.json

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //checking files
+    this->initFiles();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::initFiles()
+{
+    QDir dir(this->c_configPath);
+    //dir check
+    if (!dir.exists())
+        dir.mkpath(this->c_configPath);
+    const QString taskFilePath = c_configPath + "/"
+        + c_taskFileName;
+    QFile taskFile(taskFilePath);
+    //check task files
+    if (!taskFile.exists())
+    {
+        //if task file does not exist,make a new one
+        taskFile.open(QIODevice::ReadWrite);
+        taskFile.close();
+    }
+    qDebug() << this->c_configPath;
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStandardItemModel>
+#include <QDir>
+
+#include "backupinfo.h"
+
+#define DEBUG
+#ifdef DEBUG
+#include <QDebug>
+#endif // DEBUG
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,8 +24,26 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
 private:
+    //read tasks form global task file
+    void readTasks();
+    //write tasks to global task file
+    void writeTasks();
+    //check the system folder and file
+    void initFiles();
+private:
+    //software config path in different OS
+#ifdef Q_OS_WIN32//Windows
+    const QString c_configPath = "C:/\/ProgramData/BackupFolders";
+#elif defined Q_OS_LINUX//Linux
+    const QString c_configPath = "/usr/share/BackupFolders";
+#endif
+    const QString c_taskFileName = "tasks.json";
+    //MainWindow UI
     Ui::MainWindow *ui;
+    //item model of list view
+    QStandardItemModel* m_model;
+    //backup info list
+    QList<BackupInfo> m_infoList;
 };
 #endif // MAINWINDOW_H

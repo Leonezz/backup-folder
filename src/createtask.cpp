2ad0b2680ce51f2a1e21d07f42d82959cec9b8c9
Leonezz <lonelywolf12138@Outlook.com>
2020-11-02 20:54:51 +0800

    :construction:(createtask): basic funtion of createtask dialog:dir path selection

--- src/createtask.cpp
+++ src/createtask.cpp
@@ -4,7 +4,43 @@
 CreateTask::CreateTask(QWidget* parent)
 	: QDialog(parent)
 	, ui(new Ui::Dialog)
+	, m_syncDurationMinutes(10)
 {
 	ui->setupUi(this);
 	this->setFixedSize(QSize(400,210));
+
+	//init all the connections
+	initConnections();
+}
+
+void CreateTask::selectDestDir()
+{
+	m_destDirPath = QFileDialog::getExistingDirectory(
+		this,
+		tr("选择备份文件夹"),
+		"/"	//select from "/"
+	);
+	//show the path in the lineEdit
+	ui->lineEditDestDir->setText(m_destDirPath);
+}
+
+void CreateTask::initConnections()
+{
+	//source
+	QObject::connect(ui->pushButtonChooseSourceDir, &QPushButton::clicked
+		, this, &CreateTask::selectSourceDir);
+	//destination
+	QObject::connect(ui->pushButtonChooseDestDir, &QPushButton::clicked
+		, this, &CreateTask::selectDestDir);
+}
+
+void CreateTask::selectSourceDir()
+{
+	m_souceDirPath = QFileDialog::getExistingDirectory(
+		this,
+		tr("选择要备份的文件夹"),
+		"/"	//select from "/"
+	);
+	//show the path in the lineEdit
+	ui->lineEditsourceDir->setText(m_souceDirPath);
 }
--- src/createtask.h
+++ src/createtask.h
@@ -1,16 +1,29 @@
 #ifndef CREATETASK_H
 #define CREATETASK_H
 #include <QDialog>
-
+#include <QFileDialog>
 QT_BEGIN_NAMESPACE
 namespace Ui { class Dialog; }
 QT_END_NAMESPACE
 class CreateTask :
     public QDialog
 {
+    Q_OBJECT
 public:
     CreateTask(QWidget* parent = nullptr);
+public slots:
+    void selectSourceDir();
+    void selectDestDir();
+private:
+    //init all the connections from buttons to slots
+    void initConnections();
 private:
     Ui::Dialog* ui;
+    //source dir path from lineEdit
+    QString m_souceDirPath;
+    //destination dir path form lineEdit
+    QString m_destDirPath;
+    //sync duration time from timeEdit
+    int m_syncDurationMinutes;
 };
 #endif //CREATETASK_H
